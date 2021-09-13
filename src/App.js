9555d0c4bc6bb93296a7d345df49cedcf60cf408
joshraiborde <joshraiborde@gmail.com>
2021-09-13 13:13:20 -0400

    added links to the page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import NavBar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
