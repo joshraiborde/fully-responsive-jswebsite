71533cbda0e59189bf643d9c86854af9c5f16c4c
joshraiborde <joshraiborde@gmail.com>
2021-09-14 12:13:58 -0400

    relabeled

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
-import NavBar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <NavBar />
+      <Navbar />
       <Switch>
         <Route path='/' exact />
       </Switch>
