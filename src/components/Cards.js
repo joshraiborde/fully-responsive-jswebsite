77052ea5e6415cffeec0820725712235ab2654bc
joshraiborde <joshraiborde@gmail.com>
2021-09-16 11:08:40 -0400

    imported images from the image folder

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,25 +1,54 @@
-import React from 'react'
-import CardItem from './CardItem'
+import React from 'react';
 import './Cards.css';
+import CardItem from './CardItem';
+import img1 from './images/img-1.jpg';
+import img2 from './images/img-2.jpg';
+import img8 from './images/img-8.jpg';
 
 function Cards() {
-    return (
-        <div className="cards">
-            <h1>Check out these epic destinations</h1>
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className="cards__items">
-                    <CardItem
-                        src="images/img-8.jpg"
-                        text="Explore the hidden waterfall, deep inside the Amazon Jungle"
-                        label="Adventure"
-                        path="/services"
-                    />
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div className='cards'>
+      <h1>Check out these EPIC Destinations!</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src={img1}
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+            />
+            <CardItem
+              src={img2}
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+              src={img8}
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <CardItem
+              src={img1}
+              text='Experience Football on Top of the Himilayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <CardItem
+              src={img2}
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
