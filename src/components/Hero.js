9296f30d710303f16d19f2937a77b2f963e59c49
joshraiborde <joshraiborde@gmail.com>
2021-09-16 11:09:13 -0400

    deleted extra space

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,24 +3,31 @@ import '../App.css';
 import { Button } from './Button';
 import './Hero.css';
 
-
 function Hero() {
-    return (
-        <div className='hero-container' >
-        {/* <img src='/images/img-1.jpg' /> */}
-            <video src='/videos/video-2.mp4' autoPlay loop muted />
-            <h1>ADVENTURE AWAITS</h1>
-            <p>What are you waiting for?</p>
-            <div className="hero-btns">
-                <Button className="btns" buttonStyle='btn--outline'
-                buttonSize='btn--large'>GET STARTED</Button>
-                <Button className="btns" buttonStyle='btn--primary'
-                buttonSize='btn--large'>Watch Trailer <i className='far fa-play-circle'/>
-                
-                </Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,48 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { Button } from './Button';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
-
 function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    const showButton = () => {
-        if (window.innerWidth <= 960) {
-          setButton(false);
-        } else {
-          setButton(true);
-        }
-      };
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
 
-      useEffect(() => {
-        showButton();
-      }, []);
-    
-      window.addEventListener('resize', showButton);
+  useEffect(() => {
+    showButton();
+  }, []);
 
-    return (
-        <div>
-            {/* <> */}
-            <nav className="navbar">
-                <div className="navbar-container">
-                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                        SplashThat <i className='fab fa-typo3' />
-                    </Link> 
-                    <div className='menu-icon' onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-                    </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
+  window.addEventListener('resize', showButton);
+
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+            SplashThat
+            <i class='fab fa-typo3' />
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
               <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
-                        <li className='nav-item' >
-                        <Link
+            <li className='nav-item'>
+              <Link
                 to='/services'
                 className='nav-links'
                 onClick={closeMobileMenu}
@@ -74,9 +73,8 @@ function Navbar() {
           {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
         </div>
       </nav>
-            {/* </> */}
-        </div>
-    )
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
