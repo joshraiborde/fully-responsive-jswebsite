475bc26d96821a3c98d9ca47343ca289503474a1
joshraiborde <joshraiborde@gmail.com>
2021-09-19 17:22:08 -0400

    imported and routed services, proudcts, signup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
-
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+// 2021-09-16 imported and routed services, proudcts, signup
 function App() {
   return (
     <div className="App">
@@ -12,6 +15,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services}/>
+        <Route path='/products' component={Products}/>
+        <Route path='/sign-up' component={SignUp}/>
       </Switch>
       </Router>
     </div>
